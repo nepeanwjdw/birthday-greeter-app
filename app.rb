026986f72cd1306a2e37ec8b1788a47081e18346
Will Wayman <will.jdw@gmail.com>
2018-11-15 10:14:25 +0000

    testing infrastructure

--- app.rb
+++ app.rb
@@ -2,4 +2,19 @@ require 'sinatra/base'
 
 class BirthdayGreeter < Sinatra::Base
 
+  enable :sessions
+
+  get '/' do
+    erb :index
+  end
+
+  get '/happybirthday' do
+    erb :happybirthday
+  end
+
+  get '/countdown' do
+    erb :countdown
+  end
+
+  run! if app_file == $0
 end
---
+++ spec/features/enter_details_spec.rb
@@ -0,0 +1,5 @@
+feature "Testing the infrastructure" do
+  scenario "test" do
+    expect(page).to have_text "testing infrastructure"
+  end
+end
