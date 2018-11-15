3475de0ef5efffb75a67adf39ea3eb75699fd0be
Will Wayman <will.jdw@gmail.com>
2018-11-15 11:46:40 +0000

    feature test for countdown

--- spec/features/enter_details_spec.rb
+++ spec/features/enter_details_spec.rb
@@ -8,3 +8,14 @@ feature "It is your Birthday" do
     expect(page).to have_text "Happy Birthday Will"
   end
 end
+
+feature "It is not your Birthday" do
+  scenario "tells you how many days until your Birthday" do
+    visit '/'
+    fill_in "name", with: "Will"
+    fill_in "day", with: "13"
+    fill_in "month", with: "November"
+    click_button "Submit"
+    expect(page).to have_text "Your Birthday will be in 2 days, Will"
+  end
+end
